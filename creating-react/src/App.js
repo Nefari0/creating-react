85d2378498e2bc2f68350a84caf834667137a83f
Nefari0 <chrisdemontigny@yahoo.com>
2022-11-25 07:51:42 -0700

    more changes

--- creating-react/src/App.js
+++ creating-react/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
           <h1>prototype</h1>
           <h2>changes</h2>
+          <h2>more changes</h2>
       </header>
     </div>
   );
