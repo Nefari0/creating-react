ba158cbb33eb6ab8c1c29363722c1017a7910f97
Nefari0 <chrisdemontigny@yahoo.com>
2022-11-25 07:58:48 -0700

    removed changes

--- creating-react/src/App.js
+++ creating-react/src/App.js
@@ -7,8 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
           <h1>prototype</h1>
-          <h2>changes</h2>
-          <h2>more changes</h2>
       </header>
     </div>
   );
